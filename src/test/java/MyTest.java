6ab88693c52fb66c1df7918b914f13e5e3534e10
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-09-28 18:14:18 +0300

    done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void basicTests() {
+        assertEquals("Hello boss", Kata.greet("Daniel", "Daniel"), "For inputs \"Daniel\" and \"Daniel\"");
+        assertEquals("Hello guest", Kata.greet("Greg", "Daniel"), "For inputs \"Greg\" and \"Daniel\"");
+    }
 
+    private static class Kata {
+        public static String greet(String name, String owner) {
+            return name.equals(owner) ? "Hello boss" : "Hello guest";
+        }
     }
 }
